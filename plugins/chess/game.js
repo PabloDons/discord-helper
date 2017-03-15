061e4b87d3588b025ce5148df83a99218efa890f
PabloDons <omar.saad0705@gmail.com>
2017-03-15 20:19:06 +0100

    bug fix bug fix bug fix so much dumb shit

--- plugins/chess/game.js
+++ plugins/chess/game.js
@@ -6,22 +6,26 @@ exports.commands = [
 ];
 
 const spawn = require("child_process").spawn;
-const boardgen = spawn('python', ['./boardimage/server.py']);
+const boardgen = spawn('python', ['plugins/chess/boardgen/server.py']);
 boardgen.stdout.on('data', (chunk)=>{
-    msg.channel.stopTyping();
-    boardgenqueue.shift().sendFile(chunk, "board.png");
+    var ch = boardgenqueue.shift();
+    ch.stopTyping();
+    ch.sendFile(chunk, "board.png");
+});
+boardgen.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
 });
 var boardgenqueue = [];
 
 const Chess = require('chess.js').Chess;
 var runningGames = [];
 var gameId = 0;
-function game(ch, white, black, timeout) {
+
+function Game(ch, white, black) {
     this.id = (gameId+=1);
 
     this.channel = ch;
     this.pending = true;
-    this.acceptTimeout = timeout;
     this.w = white;
     this.b = black;
 
@@ -34,7 +38,7 @@ exports.startchess = {
     process: function(bot, msg, suffix, config) {
         var id;
         if (suffix.length>4) {
-            id = suffix.substring(3,suffix.length-1);
+            id = suffix.substring(2,suffix.length-1);
         } else {
             throw new Error("invalid argument");
         }
@@ -43,20 +47,21 @@ exports.startchess = {
             throw new Error("User not found");
         }
 
+        console.log(challengee.username);
+
         var game = runningGames.find((el)=>(el.b == msg.author || el.w == challengee));
         if (typeof game != 'undefined') {
             msg.channel.sendMessage("You or your challengee is already in a game in "+game.channel+"! End or finish it first");
             throw Error("Already in game");
         }
-        var challenger = challengee;
-        msg.channel.sendMessage(challenger+" Accept with `"+config.commandPrefix+"acceptchess` within 1 minute")
+        msg.channel.sendMessage(challengee+" Accept with `"+config.commandPrefix+"acceptchess` within 1 minute")
         .then((chall)=>{
-            var game = new game(msg.channel, challengee, msg.author, chessTimeout);
-            runningGames.push(game);
-            var chessTimeout = bot.setTimeout(()=>{
-                chall.edit(challenger+" Challenge expired!");
+            var game = new Game(msg.channel, challengee, msg.author);
+            game.acceptTimeout = bot.setTimeout(()=>{
+                chall.edit(msg.author+" Challenge expired!");
                 runningGames.splice(runningGames.findIndex((el)=>el.id==game.id), 1);
             }, 60000);
+            runningGames.push(game);
         });
     }
 };
@@ -65,25 +70,25 @@ exports.acceptchess = {
     usage: "",
     describtion: "accept a challenge",
     process: function(bot, msg, suffix) {
-        var game = runningGames.find((el)=>(el.w.id == msg.author.id));
+        var game = runningGames.find(function(el) {return (el.w.id == msg.author.id);});
         if (typeof game == 'undefined') {
             msg.channel.sendMessage("You have not been challenged to play any game!");
         } else if (!game.pending) {
             msg.channel.sendMessage("You are already in a game in "+game.channel+"! End or finish it first");
         } else {
             game.pending = false;
-            bot.clearTimeout(game.timeout);
+            bot.clearTimeout(game.acceptTimeout);
             msg.channel.sendMessage("Game started between "+game.w+" and "+game.b+"!");
 
             boardgen.stdin.write(JSON.stringify({
                 type:"png",
                 render:{
-                    fen: game.fen().split(" ")[0]
+                    fen: game.chess.fen().split(" ")[0]
                 }
             })+"\n", undefined, ()=>{
                 msg.channel.startTyping();
-                boardgenqueue.append(msg.channel);
-                msg.channel.sendMessage(game.w+" Your move!");
+                boardgenqueue.push(msg.channel);
+                msg.channel.sendMessage(game.w+" Your move!\n"+game[game.chess.turn()]+" Your move!\n**usage:** "+config.commandPrefix+"chess "+this.usage);
             });
         }
     }
@@ -112,6 +117,7 @@ exports.chess = {
         var args = suffix.split(" ");
         if (args.length < 2) {
             msg.channel.sendMessage("insufficient arguments!\n**usage:** "+config.commandPrefix+"chess "+this.usage);
+            throw new Error("argument error");
         }
         var gamei = runningGames.findIndex((el)=>(el.channel == msg.channel && (el.b == msg.author || el.w == msg.author)));
         if (gamei != -1) {
@@ -119,10 +125,17 @@ exports.chess = {
             var move = game.chess.move({from: args[0], to: args[1]});
             if (move === null) {
                 msg.channel.sendMessage("Illegal move!");
+                return;
             }
-            boardgen.stdin.write(game.fen().split(" ")[0], undefined, ()=>{
+            boardgen.stdin.write(JSON.stringify({
+                type:"png",
+                render:{
+                    fen: game.chess.fen().split(" ")[0],
+                    lastMove: args[0]+args[1]
+                }
+            })+"\n", undefined, ()=>{
                 msg.channel.startTyping();
-                boardgenqueue.append(msg.channel);
+                boardgenqueue.push(msg.channel);
                 msg.channel.sendMessage(game[game.chess.turn()]+" Your move!\n**usage:** "+config.commandPrefix+"chess "+this.usage);
             });
         } else {
---
+++ test.js
@@ -0,0 +1,14 @@
+const spawn = require('child_process').spawn;
+const ls = spawn('python', ['./plugins/chess/boardgen/server.py']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
